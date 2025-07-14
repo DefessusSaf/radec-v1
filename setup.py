6d765c3f3099ed245a70006b8299183d4a299415
DefessusSaf <sofiasittykova@gmail.com>
2025-07-14 10:43:43 +0500

    add setup conf

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astro_pipeline",
+    version="0.1.0",
+    description="Пайплайн обработки FITS-изображений и формирования отчётов",
+    author="Saf",
+    author_email="sofiasittykova@gmail.com",
+    license="MIT",
+    package_dir={"": "astro_pipeline/main"},
+    packages=find_packages(where="astro_pipeline/main/"),
+    py_modules=[
+        "monitor",
+        "astrometry",
+        "config_setting",
+        "radec_StarObservation",
+        "radec_without_mode",
+        "region",
+    ],
+
+    include_package_data=True,
+    install_requires=[
+
+        # ... другие внешние зависимости
+    ],
+    entry_points={
+        "console_scripts": [
+            # если нужен CLI: astro-pipeline → ваша точка входа
+            "astro=monitor:main",
+        ],
+    },
+)
